dab4d974aa4c5ffe3b9cb88349c0b197d12ffef3
German Gomez-Herrero <german@innovativetravel.eu>
2017-02-28 00:37:54 +0100

    API Key

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     description=description,
     long_description=long_description,
     install_requires=[
-        "humilis>=1.0.1"],
+        "humilis>=1.0.4"],
     classifiers=[
         "Programming Language :: Python :: 3"],
     zip_safe=False,
--- tests/integration/mycode/mypkg/__init__.py
+++ tests/integration/mycode/mypkg/__init__.py
@@ -1,6 +1,9 @@
 """A dummy module for testing purposes."""
 
+import time
+
 
 def handler(event, context):
     """Echo handler."""
+    event["ts"] = time.time()
     return event
