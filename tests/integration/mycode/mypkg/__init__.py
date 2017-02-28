6d0869aa91a7b131cb20307f34abfb2c9be2d132
German Gomez-Herrero <german@innovativetravel.eu>
2017-02-28 15:05:53 +0100

    More complex integration test

--- tests/integration/mycode/mypkg/__init__.py
+++ tests/integration/mycode/mypkg/__init__.py
@@ -1,9 +1,17 @@
 """A dummy module for testing purposes."""
 
+import json
 import time
+from user_agents import parse
 
 
-def handler(event, context):
+def echo(event, context):
     """Echo handler."""
     event["ts"] = time.time()
     return event
+
+
+def uaparse(event, context):
+    """Echo handler."""
+    header = json.loads(event["parameters"]).get("header")
+    return {"device": parse(header["User-Agent"])}
