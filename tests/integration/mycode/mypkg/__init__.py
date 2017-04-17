d222dcd2db38b8c4c8d369d184c1c5b6d1e213bc
German Gomez-Herrero <german@innovativetravel.eu>
2017-04-17 22:38:31 +0200

    Support for environment variables

--- tests/integration/mycode/mypkg/__init__.py
+++ tests/integration/mycode/mypkg/__init__.py
@@ -1,6 +1,7 @@
 """A dummy module for testing purposes."""
 
 import json
+import os
 import time
 from user_agents import parse
 
@@ -8,10 +9,11 @@ from user_agents import parse
 def echo(event, context):
     """Echo handler."""
     event["ts"] = time.time()
+    event["os.environ"] = dict(os.environ)
     return event
 
 
 def uaparse(event, context):
     """Echo handler."""
     ua = event["parameters"]["header"]["User-Agent"]
-    return {"device": str(parse(ua))}
+    return {"device": str(parse(ua)), "os.environ": dict(os.environ)}
