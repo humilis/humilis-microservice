d2202dc7c1986aee10ddebebca3caf42ba0260e2
German Gomez-Herrero <german@innovativetravel.eu>
2017-06-13 19:35:30 +0200

    BUG: Firehose delivery

--- humilis_microservice/lambda_function/handler/__init__.py
+++ humilis_microservice/lambda_function/handler/__init__.py
@@ -8,16 +8,16 @@ from lambdautils.monitor import sentry_monitor
 
 
 HANDLER = import_string('{{handler}}')
-INPUT_FIREHOSE_DELIVERY_STREAM = "{{input_firehose_delivery_stream}}"
-OUTPUT_FIREHOSE_DELIVERY_STREAM = "{{output_firehose_delivery_stream}}"
+INPUT_DELIVERY_STREAM = "{{input_delivery_stream}}"
+OUTPUT_DELIVERY_STREAM = "{{output_delivery_stream}}"
 
 
 @sentry_monitor()
 def lambda_handler(event, context):
     """Handle incoming requests."""
-    if INPUT_FIREHOSE_DELIVERY_STREAM:
-        send_to_delivery_stream(event, INPUT_FIREHOSE_DELIVERY_STREAM)
+    if INPUT_DELIVERY_STREAM:
+        send_to_delivery_stream(event, INPUT_DELIVERY_STREAM)
     resp = HANDLER(event, context)
-    if OUTPUT_FIREHOSE_DELIVERY_STREAM:
-        send_to_delivery_stream(resp, OUTPUT_FIREHOSE_DELIVERY_STREAM)
+    if OUTPUT_DELIVERY_STREAM:
+        send_to_delivery_stream(resp, OUTPUT_DELIVERY_STREAM)
     return resp
